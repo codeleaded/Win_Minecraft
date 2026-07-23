c39772d9b628a47d1a5c0d77923716fbad59be5d
codeleaded <gnu.c.reaper@gmail.com>
2026-07-24 01:39:24 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -1,5 +1,5 @@
-//#include "C:/Wichtig/System/Static/Library/WindowEngine1.0.h"
-#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+//#include "C:/Wichtig/System/Static/Library/WindowEngine.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine.h"
 #include "/home/codeleaded/System/Static/Library/Random.h"
 #include "/home/codeleaded/System/Static/Library/PerlinNoise.h"
 #include "/home/codeleaded/System/Static/Library/RayCast.h"
